9f4be9b189383b54453a08c74558f489ad3e6dbe
mhmoud <mhmoudsoliman41@outlook.com>
2022-03-26 13:16:36 +0200

    update login commit

--- lib/main.dart
+++ lib/main.dart
@@ -54,7 +54,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: AddAccountScreen(),
+        home: Wrapper(),
 
         routes: {
           'login' : (context) => login(),
